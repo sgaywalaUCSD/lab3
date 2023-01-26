6e066422ade1647f4b7ce87a71dda3264c445aa6
sgaywalaUCSD <sgaywala@ucsd.edu>
2023-01-26 12:53:57 -0800

    Fixed Reverse Methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,9 +4,9 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+        int[] input1 = { 3 };
+        ArrayExamples.reverseInPlace(input1);
+        assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
 
@@ -15,4 +15,25 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+
+  @Test 
+	public void testReverseInPlace2() {
+        int[] input1 = { 3, 7, 9 };
+        ArrayExamples.reverseInPlace(input1);
+        assertArrayEquals(new int[]{ 9, 7, 3 }, input1);
+
+        int[] input2 = { 3, 7, 9 , 11};
+        ArrayExamples.reverseInPlace(input2);
+        assertArrayEquals(new int[]{ 11, 9, 7, 3 }, input2);
+	}
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = { 3, 7, 9 };
+    assertArrayEquals(new int[]{9, 7, 3}, ArrayExamples.reversed(input1));
+    int[] input2 = { 3, 7, 9 , 11};
+    assertArrayEquals(new int[]{11, 9, 7, 3}, ArrayExamples.reversed(input2));
+  }
+
 }
